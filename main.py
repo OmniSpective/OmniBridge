30ce28317e9221c64af5e469b086377cecc596ae
Liav A <liavadato@gmail.com>
2023-04-05 16:48:05 +0300

    allow prompt to be passed as command line arguement to dall-e and chatgpt

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from wrappers.chatgpt.gpt_wrapper import GPTConfiguration, GPTWrapper
+import argparse
 import os
 
 
 # Temp file for testing requests
 
+parser = argparse.ArgumentParser(description='ChatGPT integration tool.')
+parser.add_argument('-p', '--prompt', help="prompt for chatgpt", default="hello")
+
+args = vars(parser.parse_args())
 
 config = GPTConfiguration(model='gpt-3.5-turbo', api_key=os.getenv("OPENAI_API_KEY"))
-wrapper = GPTWrapper(prompt="hello", configuration=config)
+wrapper = GPTWrapper(prompt=args["prompt"], configuration=config)
 
 print(wrapper())
