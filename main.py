e9cb1a5f763ce10fb2cfe98469670b6d688c69ac
Eliran <eliran9692@gmail.com>
2023-04-05 17:16:52 +0300

    take changes from main

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 from wrappers.api_based_wrappers.gpt_wrapper import GPTConfiguration, GPTWrapper
+import argparse
 import os
 
 # ---------------- CHATGPT ----------------------------
+parser = argparse.ArgumentParser(description='ChatGPT integration tool.')
+parser.add_argument('-p', '--prompt', help="prompt for chatgpt", default="hello")
+
+args = vars(parser.parse_args())
+
 config = GPTConfiguration(model='gpt-3.5-turbo', api_key="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")
-wrapper = GPTWrapper(prompt="hello", configuration=config)
+wrapper = GPTWrapper(prompt=args["prompt"], configuration=config)
 
 print(wrapper())
 
 # ---------------- DALLE ----------------------------
 from wrappers.api_based_wrappers.dalle_wrapper import DALLEWrapper, DALLEConfiguration
 
+parser = argparse.ArgumentParser(description='DALL-E integration tool.')
+parser.add_argument('-p', '--prompt', help="prompt for DALL-E", default="dog")
+
+args = vars(parser.parse_args())
+
 config = DALLEConfiguration(api_key="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",
                             resolution='256x256',
                             images=4)
-wrapper = DALLEWrapper(prompt="dog", configuration=config)
+wrapper = DALLEWrapper(prompt=args["prompt"], configuration=config)
 wrapper()
 
 # ---------------- HUGGINGFACE ----------------------------
