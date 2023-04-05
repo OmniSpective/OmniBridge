8db84de4bbee58d8c3ef21a6c482110f7a90b8be
Coby Penso <coby.penso24@gmail.com>
2023-04-05 15:44:19 +0300

    hugging face inference api support

--- wrappers/api_based_wrappers/hugging_face_wrapper.py
+++ wrappers/api_based_wrappers/hugging_face_wrapper.py
@@ -1,31 +1,9 @@
-# import requests
-# import os
-
-# def query(payload, model_id, api_token):
-# 	headers = {"Authorization": f"Bearer {api_token}"}
-# 	API_URL = f"https://api-inference.huggingface.co/models/{model_id}"
-# 	response = requests.post(API_URL, headers=headers, json=payload)
-# 	return response.json()
-
-
-
-# model_id = "distilbert-base-uncased"
-# api_token = os.getenv("HUGGING_FACE_API_TOKEN") # get yours at hf.co/settings/tokens
-# data = query("The goal of life is [MASK].", model_id, api_token)
-
-# print(data)
-
-
-
-
-from typing import Literal, TypeVar, TypedDict, Dict, Any
+from typing import TypedDict, Dict, Any
 import json
-import requests
 from dataclasses import dataclass
 from .base_api_wrapper import RestAPIWrapper, BaseConfiguration
 
 COMPLETIONS_API_URL = "https://api-inference.huggingface.co/models"
-dalleModel = TypeVar('dalleModel')
 
 class GPTWrapperException(Exception):
     pass
