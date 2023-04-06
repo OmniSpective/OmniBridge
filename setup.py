f333efc5b5943b49e1d4db0447def5f0a47f5c94
Eliran <eliran9692@gmail.com>
2023-04-06 18:31:43 +0300

    test

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
 from setuptools import setup
-from src.cli.version import version
+# from importlib import util
 from os import path
+# import os
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
+# spec = util.spec_from_file_location(
+#     "src.cli.version", os.path.join("src", "cli", "version.py")
+# )
+# mod = util.module_from_spec(spec)
+# spec.loader.exec_module(mod)
+# version = mod.version
+
 
 setup(
     name='OmniBridge',
     license="Apache License 2.0",
-    version=version,
+    version="0.1.5",
     author='OmniSpective',
     author_email='eliran9692@gmail.com',
     description='Bridging AI models',
