4b0ca34d64c279ccee683d5bf4cb7e0b43ae01e0
Eliran <eliran9692@gmail.com>
2023-04-06 12:14:29 +0300

    bump version

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup
 from src.cli.version import version
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
 
 setup(
     name='OmniBridge',
@@ -7,6 +13,7 @@ setup(
     author='OmniSpective',
     author_email='eliran9692@gmail.com',
     description='Bridging AI models',
+    long_description=long_description,
     url='https://github.com/tmpOrgName/OmniBridge',
     packages=['src'],
     install_requires=[
