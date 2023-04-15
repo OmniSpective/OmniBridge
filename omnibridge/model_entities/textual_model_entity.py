83d1f19f40d503d001abc91b224c8505199e7741
Liav A <liavadato@gmail.com>
2023-04-15 19:37:44 +0300

    fix lint

--- omnibridge/model_entities/textual_model_entity.py
+++ omnibridge/model_entities/textual_model_entity.py
@@ -3,6 +3,7 @@ from typing import Type
 from omnibridge.model_entities.base_model_entity import BaseModelUnit
 from omnibridge.model_entities.models_io.base_model_io import ModelIO
 from omnibridge.model_entities.models_io.base_model_io import TextualIO
+from omnibridge.wrappers.wrapper_interfaces.model_wrapper import ModelWrapper
 
 
 class TextualModel(BaseModelUnit):
@@ -15,11 +16,10 @@ class TextualModel(BaseModelUnit):
     def produced_type(self) -> Type[TextualIO]:
         return TextualIO
 
-    def process(self, model_input: TextualIO) -> TextualIO:  # type: ignore
+    def process(self, model_input: TextualIO) -> ModelIO:  # type: ignore
         if not self.can_process(model_input):
             raise Exception(f"Cannot process input of type: {type(model_input)}")
 
         prompt = model_input.get_text()
         response = self.wrapper.process(prompt=prompt)  # type: ignore
-        textual_output = TextualIO(response)
-        return textual_output
+        return response
