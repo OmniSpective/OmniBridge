ecf6e77506bcaa7a2d4e901a62c0d5f994979939
Coby Penso <coby.penso24@gmail.com>
2023-04-15 15:05:32 +0300

    register wrapper

--- omnibridge/wrappers/wrapper_instances/type_name_to_wrapper.py
+++ omnibridge/wrappers/wrapper_instances/type_name_to_wrapper.py
@@ -1,6 +1,15 @@
+from omnibridge.wrappers.wrapper_instances.dalle_wrapper import DALLEWrapper
+from omnibridge.wrappers.wrapper_instances.gpt_wrapper import GPTWrapper
+from omnibridge.wrappers.wrapper_instances.hugging_face_wrapper import HuggingFaceWrapper
+
+
 def register_wrapper(cls):
     type_names[cls.get_class_type_field()] = cls
     return cls
 
 
-type_names = {}
+type_names = {
+    GPTWrapper.get_class_type_field(): GPTWrapper,
+    DALLEWrapper.get_class_type_field(): DALLEWrapper,
+    HuggingFaceWrapper.get_class_type_field(): HuggingFaceWrapper
+}
