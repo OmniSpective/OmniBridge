4aad53b1653d035af8b0bd89389f2062b6618ac1
Eliran <eliran9692@gmail.com>
2023-04-15 11:06:06 +0300

    fix

--- omnibridge/cli/argument_parser/argument_parser.py
+++ omnibridge/cli/argument_parser/argument_parser.py
@@ -1,16 +1,15 @@
-from argparse import ArgumentParser, _SubParsersAction
 from omnibridge.cli.argument_parser.commands import Commands
+from typing import Any
 
-
-def add_api_key_arguments(parser: _SubParsersAction[ArgumentParser]) -> ArgumentParser:
-    api_key_parser = parser.add_parser(Commands.ADD_KEY, help="Add an api key.")
+def add_api_key_arguments(parser: Any) -> Any:
+    api_key_parser = parser.add_parser(Commands.ADD_KEY.value, help="Add an api key.")
     api_key_parser.add_argument('-n', '--name', help="name of the api key.", type=str, required=True)
     api_key_parser.add_argument('-v', '--value', help="value of the api key.", type=str, required=True)
     return api_key_parser
 
 
-def add_chatgpt_arguments(parser: _SubParsersAction[ArgumentParser]) -> ArgumentParser:
-    add_chatgpt_model_parser = parser.add_parser(Commands.ADD_CHATGPT, help="add chatgpt model connection details.")
+def add_chatgpt_arguments(parser: Any) -> Any:
+    add_chatgpt_model_parser = parser.add_parser(Commands.ADD_CHATGPT.value, help="add chatgpt model connection details.")
     add_chatgpt_model_parser.add_argument('-n', '--name', type=str, required=True,
                                           help="name of the model, e.g. my_gpt4.")
     add_chatgpt_model_parser.add_argument('-k', '--key', type=str, required=True, help="api key name.")
@@ -19,8 +18,8 @@ def add_chatgpt_arguments(parser: _SubParsersAction[ArgumentParser]) -> Argument
     return add_chatgpt_model_parser
 
 
-def add_dalle_arguments(parser: _SubParsersAction[ArgumentParser]) -> ArgumentParser:
-    add_dalle_model_parser = parser.add_parser(Commands.ADD_DALLE, help="add dalle model.")
+def add_dalle_arguments(parser: Any) -> Any:
+    add_dalle_model_parser = parser.add_parser(Commands.ADD_DALLE.value, help="add dalle model.")
     add_dalle_model_parser.add_argument('-n', '--name', type=str, required=True, help="name of the model.")
     add_dalle_model_parser.add_argument('-k', '--key', type=str, required=True, help="api key name.")
     add_dalle_model_parser.add_argument('--num-images', type=str, default="4",
