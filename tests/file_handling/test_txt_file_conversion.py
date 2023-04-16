57efb4dbf5759cfa0b24fdb5283351085c0ac969
Liav A <liavadato@gmail.com>
2023-04-16 13:50:32 +0300

    add conversion from txt file to textio

---
+++ tests/file_handling/test_txt_file_conversion.py
@@ -0,0 +1,18 @@
+import os
+
+from omnibridge.model_entities.models_io.file_to_text_converter import FileInputHandler
+
+
+def test_txt_file_conversion_to_textual_io():
+    # Arrange
+    content = 'Hello, World!'
+    file_name = 'hello_for_test.txt'
+    with open(file_name, 'w') as f:
+        f.write(content)
+
+    # Act
+    text_io = FileInputHandler.convert_file(file_name)
+    os.remove(file_name)  # cleanup
+
+    # Assert
+    assert text_io.get_text() == content
