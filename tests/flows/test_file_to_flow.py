f981858f6adbf366283961c10ef5a297ae7090b2
Coby Penso <coby.penso24@gmail.com>
2023-04-16 08:56:30 +0300

    checkpoint

---
+++ tests/flows/test_file_to_flow.py
@@ -0,0 +1,42 @@
+from typing import Dict, List
+from omnibridge.flows.file_to_flow import read_flow_from_json, read_flow_from_yaml
+from omnibridge.flows.flow_types import identify_dag_type, DagType
+
+def test_json_to_dict():
+    file_path = 'test_flows/seq_flow.json'
+    flow: Dict[str, List[str]] = read_flow_from_json(file_path=file_path)
+    assert flow.keys() == ['step1', 'step2']
+    assert len(flow['step1']) == 0
+    assert len(flow['step2']) == 1
+    assert flow['step2'] == 'step1'
+
+def test_yaml_to_dict():
+    file_path = 'test_flows/seq_flow.yaml'
+    flow: Dict[str, List[str]] = read_flow_from_yaml(file_path=file_path)
+    assert flow.keys() == ['step1', 'step2']
+    assert len(flow['step1']) == 0
+    assert len(flow['step2']) == 1
+    assert flow['step2'] == 'step1'
+
+
+def test_dict_to_flow():
+    pass
+
+
+def test_flow_type_identification():
+    file_path = 'test_flows/seq_flow.yaml'
+    flow: Dict[str, List[str]] = read_flow_from_yaml(file_path=file_path)
+    flow_type: DagType = identify_dag_type(flow)
+    assert flow_type == DagType.SEQUENTIAL
+
+
+    file_path = 'test_flows/seq_flow.json'
+    flow: Dict[str, List[str]] = read_flow_from_json(file_path=file_path)
+    flow_type: DagType = identify_dag_type(flow)
+    assert flow_type == DagType.SEQUENTIAL
+
+
+    file_path = 'test_flows/branch.json'
+    flow: Dict[str, List[str]] = read_flow_from_json(file_path=file_path)
+    flow_type: DagType = identify_dag_type(flow)
+    assert flow_type == DagType.BRANCHING
