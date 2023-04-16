1823631a424af506f28becb99e901f668c257c62
Eliran <eliran9692@gmail.com>
2023-04-16 21:12:02 +0300

    list command

--- omnibridge/main.py
+++ omnibridge/main.py
@@ -4,10 +4,10 @@ from omnibridge.cli.banner import banner
 from omnibridge.cli.create.create_parser import add_create_key_sub_parser, \
     add_create_flow_sub_parser, add_create_model_sub_parser
 from omnibridge.cli.run.run_parser import add_run_flow_sub_parser
-from omnibridge.cli.list.list_parser import add_list_wrappers_arguments
+from omnibridge.cli.list.list_parser import add_list_arguments
 from omnibridge.cli.create.create_command_handler import handle_create_command
 from omnibridge.cli.run.run_command_handler import handle_run_flow_command
-from omnibridge.cli.list.list_command_handler import handle_list_wrappers_command
+from omnibridge.cli.list.list_command_handler import handle_list_command
 from omnibridge.wrappers.wrapper_instances.type_name_to_wrapper import ModelLoader
 
 
@@ -17,21 +17,25 @@ def run() -> int:
     parser.add_argument('-n', "--model-name", help="which model to use.")
 
     subparsers = parser.add_subparsers(help='sub-command help', dest='command')
+
     create_parser = subparsers.add_parser("create", help="Create models/keys/flows.")
     create_subparsers = create_parser.add_subparsers(help='create sub-command help', dest='object_to_create')
+
+    list_parser = subparsers.add_parser("list", help="List keys/models/flows/wrappers")
+    list_subparsers = list_parser.add_subparsers(help="list sub-command help", dest="object_to_list")
 
     add_create_key_sub_parser(create_subparsers)
     add_create_model_sub_parser(create_subparsers)
     add_create_flow_sub_parser(create_subparsers)
     add_run_flow_sub_parser(subparsers)
-    add_list_wrappers_arguments(subparsers)
+    add_list_arguments(list_subparsers)
     
     args = vars(parser.parse_args())
 
     if args['command'] == 'create':
         handle_create_command(args)
-    elif args['command'] == 'list-wrappers':
-        handle_list_wrappers_command()
+    elif args['command'] == 'list':
+        handle_list_command(args)
     elif args['command'] == 'run-flow':
         handle_run_flow_command(args)
     elif args['prompt'] and args['model_name']:
