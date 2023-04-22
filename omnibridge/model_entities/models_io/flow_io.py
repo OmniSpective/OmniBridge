44a47b0d0200989cda257ab4c9c04ba2e904d68e
Liav A <liavadato@gmail.com>
2023-04-22 22:15:59 +0300

    refactor so FlowIO doesn't include unused fields

--- omnibridge/model_entities/models_io/flow_io.py
+++ omnibridge/model_entities/models_io/flow_io.py
@@ -8,18 +8,14 @@ class FlowIO(ModelIO):
         self.text = ""
         self.image_paths = []
         self.pretty_text = ""
-        self._text_outputs_processed = 0
-        self._image_outputs_processed = 0
 
         if isinstance(model_io, TextualIO):
             self.text = model_io.get_text()
             self.pretty_text = self.text
-            self._text_outputs_processed += 1
 
         elif isinstance(model_io, ImageIO):
             self.image_paths = model_io.image_paths()
             self.pretty_text = repr(model_io)
-            self._image_outputs_processed += 1
 
         else:
             raise NotImplementedError()
@@ -33,9 +29,6 @@ class FlowIO(ModelIO):
     def get_image_paths(self) -> List[str]:
         return self.image_paths
 
-    def get_inputs_aggregated_amount(self) -> int:
-        return self._text_outputs_processed + self._image_outputs_processed
-
     def __repr__(self) -> str:
         return self.get_pretty_text()
 
@@ -45,21 +38,17 @@ class FlowIO(ModelIO):
         if isinstance(other, TextualIO):
             self.text += '\n' + other.get_text()
             self.pretty_text += line_seperator + other.get_text()
-            self._text_outputs_processed += 1
             return self
 
         elif isinstance(other, ImageIO):
             self.image_paths += other.image_paths()
             self.pretty_text += line_seperator + repr(other)
-            self._image_outputs_processed += 1
             return self
 
         elif isinstance(other, FlowIO):
             self.text += '\n' + other.get_text()
             self.image_paths += other.get_image_paths()
             self.pretty_text += line_seperator + other.get_pretty_text()
-            self._image_outputs_processed += other._image_outputs_processed
-            self._text_outputs_processed += other._text_outputs_processed
             return self
 
         else:
--- tests/unit/flows/test_graph_flow.py
+++ tests/unit/flows/test_graph_flow.py
@@ -4,64 +4,70 @@ from omnibridge.wrappers.wrapper_interfaces.processor import Processor
 
 
 class MockProcessor(Processor):
+    processed = 0
     output = "test_output"
 
     def __init__(self, return_value: ModelIO = TextualIO(output)):
         self.return_value = return_value
 
     def process(self, model_input: ModelIO) -> ModelIO:
+        MockProcessor.processed += 1
         return self.return_value
 
 
 def test_single_node_graph_flow():
     # Arrange
+    MockProcessor.processed = 0
     node = Node(MockProcessor())
     flow = GraphFlow([node])
     flow_input = TextualIO("test_input")
 
     # Act
-    flow_output = flow.process(flow_input)
+    flow.process(flow_input)
 
     # Assert
-    assert flow_output.get_inputs_aggregated_amount() == 1
+    assert MockProcessor.processed == 1
 
 
 def test_chained_node_graph_flow():
     # Arrange
+    MockProcessor.processed = 0
     last_node = Node(MockProcessor())
     node = Node(MockProcessor(), "", [last_node])
     flow = GraphFlow([node])
     flow_input = TextualIO("test_input")
 
     # Act
-    flow_output = flow.process(flow_input)
+    flow.process(flow_input)
 
     # Assert
-    assert flow_output.get_inputs_aggregated_amount() == 2
+    assert MockProcessor.processed == 2
 
 
 def test_parallel_node_graph_flow():
     # Arrange
+    MockProcessor.processed = 0
     node = Node(MockProcessor())
     flow = GraphFlow([node, node])
     flow_input = TextualIO("test_input")
 
     # Act
-    flow_output = flow.process(flow_input)
+    flow.process(flow_input)
 
     # Assert
-    assert flow_output.get_inputs_aggregated_amount() == 2
+    assert MockProcessor.processed == 2
 
 
 def test_complex_graph_flow():
     # Arrange
+    MockProcessor.processed = 0
     last_nodes = Node(MockProcessor(ImageIO(["./file1", "./file2"])))
     nodes = [Node(MockProcessor(), instruction="", next_nodes=[last_nodes]) for _ in range(2)]
     flow = GraphFlow(nodes)
     flow_input = TextualIO("test_input")
 
     # Act
-    flow_output = flow.process(flow_input)
+    flow.process(flow_input)
 
     # Assert
-    assert flow_output.get_inputs_aggregated_amount() == 4
+    assert MockProcessor.processed == 4
